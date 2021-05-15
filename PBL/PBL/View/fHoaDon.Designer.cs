b8ec686cd8f53b8804958bd83c0264a85bfffc1e
HunineZ <hqhung201@gmail.com>
2021-05-15 18:39:53 +0700

    edit lai giao dien

--- PBL/PBL/View/fHoaDon.Designer.cs
+++ PBL/PBL/View/fHoaDon.Designer.cs
@@ -53,37 +53,42 @@ namespace PBL.View
             // 
             // dgvHoaDon
             // 
+            this.dgvHoaDon.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             this.dgvHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHoaDon.Location = new System.Drawing.Point(4, 3);
+            this.dgvHoaDon.Location = new System.Drawing.Point(3, 2);
+            this.dgvHoaDon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvHoaDon.Name = "dgvHoaDon";
             this.dgvHoaDon.RowHeadersWidth = 51;
             this.dgvHoaDon.RowTemplate.Height = 24;
             this.dgvHoaDon.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvHoaDon.Size = new System.Drawing.Size(780, 538);
+            this.dgvHoaDon.Size = new System.Drawing.Size(585, 437);
             this.dgvHoaDon.TabIndex = 0;
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.dgvHoaDon);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(9, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(787, 544);
+            this.panel1.Size = new System.Drawing.Size(590, 442);
             this.panel1.TabIndex = 1;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.btXoa);
             this.panel2.Controls.Add(this.btDetail);
-            this.panel2.Location = new System.Drawing.Point(805, 12);
+            this.panel2.Location = new System.Drawing.Point(604, 10);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(236, 86);
+            this.panel2.Size = new System.Drawing.Size(177, 70);
             this.panel2.TabIndex = 2;
             // 
             // btXoa
             // 
-            this.btXoa.Location = new System.Drawing.Point(17, 28);
+            this.btXoa.Location = new System.Drawing.Point(13, 23);
+            this.btXoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btXoa.Name = "btXoa";
-            this.btXoa.Size = new System.Drawing.Size(87, 33);
+            this.btXoa.Size = new System.Drawing.Size(65, 27);
             this.btXoa.TabIndex = 1;
             this.btXoa.Text = "Xoá";
             this.btXoa.UseVisualStyleBackColor = true;
@@ -91,9 +96,10 @@ namespace PBL.View
             // 
             // btDetail
             // 
-            this.btDetail.Location = new System.Drawing.Point(130, 28);
+            this.btDetail.Location = new System.Drawing.Point(98, 23);
+            this.btDetail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btDetail.Name = "btDetail";
-            this.btDetail.Size = new System.Drawing.Size(87, 33);
+            this.btDetail.Size = new System.Drawing.Size(65, 27);
             this.btDetail.TabIndex = 0;
             this.btDetail.Text = "Chi tiết";
             this.btDetail.UseVisualStyleBackColor = true;
@@ -105,16 +111,18 @@ namespace PBL.View
             this.panel3.Controls.Add(this.lbReset);
             this.panel3.Controls.Add(this.label1);
             this.panel3.Controls.Add(this.txbSearch);
-            this.panel3.Location = new System.Drawing.Point(805, 104);
+            this.panel3.Location = new System.Drawing.Point(604, 84);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(236, 122);
+            this.panel3.Size = new System.Drawing.Size(177, 99);
             this.panel3.TabIndex = 3;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(77, 75);
+            this.btnSearch.Location = new System.Drawing.Point(58, 61);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(87, 33);
+            this.btnSearch.Size = new System.Drawing.Size(65, 27);
             this.btnSearch.TabIndex = 2;
             this.btnSearch.Text = "Tìm";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -125,9 +133,10 @@ namespace PBL.View
             this.lbReset.AutoSize = true;
             this.lbReset.BackColor = System.Drawing.Color.White;
             this.lbReset.ForeColor = System.Drawing.Color.DarkRed;
-            this.lbReset.Location = new System.Drawing.Point(199, 49);
+            this.lbReset.Location = new System.Drawing.Point(149, 40);
+            this.lbReset.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbReset.Name = "lbReset";
-            this.lbReset.Size = new System.Drawing.Size(17, 17);
+            this.lbReset.Size = new System.Drawing.Size(14, 13);
             this.lbReset.TabIndex = 3;
             this.lbReset.Text = "X";
             this.lbReset.Click += new System.EventHandler(this.lbReset_Click);
@@ -135,17 +144,19 @@ namespace PBL.View
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 17);
+            this.label1.Location = new System.Drawing.Point(10, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 17);
+            this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nhập Mã Book:";
             // 
             // txbSearch
             // 
-            this.txbSearch.Location = new System.Drawing.Point(17, 47);
+            this.txbSearch.Location = new System.Drawing.Point(13, 38);
+            this.txbSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txbSearch.Name = "txbSearch";
-            this.txbSearch.Size = new System.Drawing.Size(200, 22);
+            this.txbSearch.Size = new System.Drawing.Size(151, 20);
             this.txbSearch.TabIndex = 0;
             this.txbSearch.TextChanged += new System.EventHandler(this.txbSearch_TextChanged);
             // 
@@ -154,16 +165,18 @@ namespace PBL.View
             this.panel4.Controls.Add(this.btReset);
             this.panel4.Controls.Add(this.cbbSort);
             this.panel4.Controls.Add(this.label2);
-            this.panel4.Location = new System.Drawing.Point(805, 232);
+            this.panel4.Location = new System.Drawing.Point(604, 188);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(236, 321);
+            this.panel4.Size = new System.Drawing.Size(177, 261);
             this.panel4.TabIndex = 4;
             // 
             // btReset
             // 
-            this.btReset.Location = new System.Drawing.Point(77, 74);
+            this.btReset.Location = new System.Drawing.Point(58, 60);
+            this.btReset.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btReset.Name = "btReset";
-            this.btReset.Size = new System.Drawing.Size(87, 33);
+            this.btReset.Size = new System.Drawing.Size(65, 27);
             this.btReset.TabIndex = 2;
             this.btReset.Text = "Reset";
             this.btReset.UseVisualStyleBackColor = true;
@@ -172,32 +185,36 @@ namespace PBL.View
             // cbbSort
             // 
             this.cbbSort.FormattingEnabled = true;
-            this.cbbSort.Location = new System.Drawing.Point(17, 44);
+            this.cbbSort.Location = new System.Drawing.Point(13, 36);
+            this.cbbSort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbbSort.Name = "cbbSort";
-            this.cbbSort.Size = new System.Drawing.Size(200, 24);
+            this.cbbSort.Size = new System.Drawing.Size(151, 21);
             this.cbbSort.TabIndex = 4;
             this.cbbSort.DropDownClosed += new System.EventHandler(this.cbbSort_DropDownClosed);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 15);
+            this.label2.Location = new System.Drawing.Point(10, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.Size = new System.Drawing.Size(73, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Sắp xếp theo:";
             // 
             // fHoaDon
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1045, 568);
+            this.ClientSize = new System.Drawing.Size(784, 462);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "fHoaDon";
-            this.Text = "fHoaDon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hóa đơn";
             ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
